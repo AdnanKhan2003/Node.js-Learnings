047d3ff5484a11d451be6f754b889a76f475d572
Adnan <adnanmkhan2003@gmail.com>
2024-10-19 23:46:45 +0530

    Added code and notes for templating engines

---
+++ 04- Dynamic Content and Adding Template Engines/1. Sharing data across Users and Requests/app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const rootDir = require('./util/path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/user')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Adding common path segments request handler middlewares in one file
+app.use('/admin', adminRoutes.routes);
+
+app.use(shopRoutes);
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(rootDir, 'view', 'page-not-found.html'));
+});
+
+app.listen(3000);
---
+++ 04- Dynamic Content and Adding Template Engines/2. Installing and implementing pug/routes/admin.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const rootDir = require('../util/path');
+
+const express = require('express');
+
+const router = express.Router();
+
+const products = [];
+// sendFile takes absolute for this project on operating system isnide ''
+// Same path but different requests
+// '/admin' would be automatically prepended
+router.get('/add-product', (req, res, next) => {
+    // res.sendFile(path.join(__dirname, '..', 'view', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+router.post('/add-product', (req, res) => {
+    products.push({ title: req.body.title });
+    res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
