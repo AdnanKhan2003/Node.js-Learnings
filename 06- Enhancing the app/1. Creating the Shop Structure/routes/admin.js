5e472c1df8ad833dab39f0333189d32577b50ed9
Adnan <adnanmkhan2003@gmail.com>
2024-10-21 23:53:38 +0530

    New topic started

---
+++ 06- Enhancing the app/1. Creating the Shop Structure/routes/admin.js
@@ -0,0 +1,11 @@
+const productsController = require('../controllers/products');
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product', productsController.getAddProduct);
+
+router.post('/add-product', productsController.postAddProduct);
+
+module.exports = router;
