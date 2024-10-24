7fa0afad438289ea3310b7eea76e1a424d2d6ca0
Adnan <adnanmkhan2003@gmail.com>
2024-10-24 23:58:32 +0530

    Deleting items functionality added

---
+++ 07- Dynamic Routes and Advance Models/8. Deleting Cart Items/models/cart.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+  );
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        // Check if cart exists
+        fs.readFile(p, (err, fileContent) => {
+        // If don't
+            let cart = { products: [], totalPrice: 0 };
+
+            // If exists
+            if(!err){
+                cart = JSON.parse(fileContent);
+            }
+
+            // Find existing product
+            const existingProductIndex = cart.products.findIndex(product => product.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+
+            let updatedProduct;
+            // Add new product/ increase existing one
+            if(existingProduct){
+                updatedProduct = { ...existingProduct };
+                updatedProduct.qty += 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
+                updatedProduct = { id, qty: 1 };
+                cart.products = [ ...cart.products, updatedProduct ];
+            }
+            cart.totalPrice += +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
+            })
+        })
+
+    }
+
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            if(err){
+                return;
+            }
+
+            const updatedCart = { ...JSON.parse(fileContent) };
+            
+            const existingProduct = updatedCart.products.find(product => product.id === id);
+            updatedCart.totalPrice -= productPrice * existingProduct.qty;
+            updatedCart.products = updatedCart.products.filter(product => product.id !== id);
+            // console.log(updatedCart.totalPrice, updatedCart.qty, productPrice, 'ngl', updatedCart);
+            
+
+            fs.writeFile(p, JSON.stringify(updatedCart), err => {
+                if(err) console.log(err);
+                
+            })
+        })
+    }
+}
